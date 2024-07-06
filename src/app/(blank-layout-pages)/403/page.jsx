18df44b3560f98dd828231e68a8a40edcfcec2b5
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-07-06 08:38:33 +0330

    add permission denied page

---
+++ src/app/(blank-layout-pages)/403/page.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+// Next Imports
+import Link from 'next/link'
+
+// MUI Imports
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+// Hook Imports
+import { useImageVariant } from '@core/hooks/useImageVariant'
+
+const Page = ({ mode }) => {
+    // Vars
+    const darkImg = '/images/pages/misc-mask-1-dark.png'
+    const lightImg = '/images/pages/misc-mask-1-light.png'
+
+    // Hooks
+    const miscBackground = useImageVariant(mode, lightImg, darkImg)
+
+    return (
+        <div className='flex items-center justify-center min-bs-[100dvh] relative p-6 overflow-x-hidden'>
+            <div className='flex items-center flex-col text-center gap-10'>
+                <div className='flex flex-col gap-2 is-[90vw] sm:is-[unset]'>
+                    <Typography className='font-medium text-8xl' color='text.primary'>
+                        403
+                    </Typography>
+                    <Typography variant='h4'>شما  به این صفحه دسترسی ندارید⚠️</Typography>
+                    <Typography></Typography>
+                </div>
+                <img
+                    alt='error-illustration'
+                    src='/images/illustrations/characters/3.png'
+                    className='object-cover bs-[400px] md:bs-[450px] lg:bs-[500px]'
+                />
+                {/*<Button href='/municipality/list' component={Link} variant='contained'>*/}
+                {/*    Back to Home*/}
+                {/*</Button>*/}
+            </div>
+            <img src={miscBackground} className='absolute bottom-0 z-[-1] is-full max-md:hidden' />
+        </div>
+    )
+}
+
+export default Page
