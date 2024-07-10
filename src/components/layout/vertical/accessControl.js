4e8432148973b4c025fdc6d1be8ea5fe0a1bea01
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-07-10 13:56:15 +0330

    set titles for items

--- src/components/layout/vertical/accessControl.js
+++ src/components/layout/vertical/accessControl.js
@@ -4,17 +4,16 @@ import WORK_GROUPS from './workGroups';
 
 const accessControl = {
     [WORK_GROUPS.CFO]: [
-        {label: 'جدول امور مالی', href: '/dehyari/cfo/table'},
-        {label: 'چارت سازمانی', href: '/dehyari'},
-        {label: 'ایجاد فرد جدید', href: '/dehyari/form'},
+        {label: 'لیست پرسنل', href: '/dehyari/cfo/table'},
+        {label: 'تشکیلات دهیاری', href: '/dehyari'},
+        {label: 'ثبت اطلاعات پرسنلی', href: '/dehyari/form'},
     ],
     [WORK_GROUPS.GOVERNOR]: [
-        {label: 'جدول استاندار', href: '/dehyari/governor/table'},
-        {label: 'ایجاد کاربر', href: '/municipality/list'},
+        {label: 'لیست پرسنل', href: '/dehyari/governor/table'},
+        {label: 'کاربران سامانه', href: '/municipality/list'},
     ],
     [WORK_GROUPS.BAKHSHDAR]: [
-        {label: 'جدول بخشدار', href: '/dehyari/bakhshdar/table'},
-        {label: 'ایجاد کاربر', href: '/municipality/list'},
+        {label: 'لیست پرسنل', href: '/dehyari/bakhshdar/table'},
     ],
     [WORK_GROUPS.PUBLIC]: [
         { label: 'صفحه عمومی', href: '/profile/complete' },
