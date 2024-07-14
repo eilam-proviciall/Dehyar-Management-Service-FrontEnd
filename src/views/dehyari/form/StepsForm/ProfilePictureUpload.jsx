2529d25ad00996b5b7b0f20d8bc062a5db8d8f88
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-07-14 20:59:30 +0330

    add validation for pictures

--- src/views/dehyari/form/StepsForm/ProfilePictureUpload.jsx
+++ src/views/dehyari/form/StepsForm/ProfilePictureUpload.jsx
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { styled } from '@mui/material/styles';
 
-// Register the plugins
 registerPlugin(FilePondPluginImagePreview, FilePondPluginFileEncode);
 
 const CustomGrid = styled(Grid)(({ theme }) => ({
@@ -43,28 +41,32 @@ const ProfilePictureUpload = ({ onNext, onBack }) => {
 
     const handleSubmit = () => {
         const values = getValues();
-        console.log(values);  // اینجا باید formData به‌روزرسانی‌شده را مشاهده کنید
         onNext();
     };
 
     return (
-                <Card>
-                    <CardContent className="flex flex-col gap-4">
-                        <CenteredGrid item xs={12}>
-                            <Typography className='font-medium' color='text.primary'>
-                                عکس پروفایل
-                            </Typography>
-                            <Typography variant='body2'>لطفا عکس پروفایل  را آپلود کنید</Typography>
-                        </CenteredGrid>
-                            <FilePond
-                                files={files}
-                                onupdatefiles={handleFileChange}
-                                allowMultiple={false}
-                                name="file"
-                                labelIdle='عکس خود را اینجا بکشید یا <span class="filepond--label-action">انتخاب کنید</span>'
-                            />
-                    </CardContent>
-                </Card>
+        <Card>
+            <CardContent className="flex flex-col gap-4">
+                <CenteredGrid item xs={12}>
+                    <Typography className='font-medium' color='text.primary'>
+                        عکس پروفایل
+                    </Typography>
+                    <Typography variant='body2'>لطفا عکس پروفایل  را آپلود کنید</Typography>
+                </CenteredGrid>
+                <FilePond
+                    files={files}
+                    onupdatefiles={handleFileChange}
+                    allowMultiple={false}
+                    name="file"
+                    labelIdle='عکس خود را اینجا بکشید یا <span class="filepond--label-action">انتخاب کنید</span>'
+                    acceptedFileTypes={['image/png', 'image/jpeg']}
+                    maxFileSize='200KB'
+                    fileValidateTypeLabelExpectedTypes='فقط فایل‌های PNG و JPG مجاز هستند'
+                    fileValidateSizeLabelMaxFileSizeExceeded='حجم فایل نباید بیشتر از 200KB باشد'
+                    fileValidateSizeLabelMaxFileSize='حجم فایل مجاز: {filesize}'
+                />
+            </CardContent>
+        </Card>
     );
 };
 
