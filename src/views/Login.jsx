15f5a5ff2a324c13cae9ebe7cdf787f89ae49cde
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-08-08 18:34:46 +0330

    handle all status bar for dehyari chart

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,11 @@
 'use client'
 
 // React Imports
-import {useContext, useState} from 'react'
+import {useContext, useState, useEffect} from 'react'
 
 // Next Imports
 import Link from 'next/link'
-import { useParams, useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 // MUI Imports
 import Typography from '@mui/material/Typography'
@@ -15,14 +15,13 @@ import InputAdornment from '@mui/material/InputAdornment'
 import Checkbox from '@mui/material/Checkbox'
 import Button from '@mui/material/Button'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import Divider from '@mui/material/Divider'
-import Alert from '@mui/material/Alert'
+import Box from '@mui/material/Box'
 
 // Third-party Imports
 import { Controller, useForm } from 'react-hook-form'
 import { valibotResolver } from '@hookform/resolvers/valibot'
-import { object, minLength, string, email } from 'valibot'
-import classnames from 'classnames'
+import { object, minLength, string } from 'valibot'
+import { AuthContext } from '@/contexts/AuthContext'
 
 // Component Imports
 import Logo from '@components/layout/shared/Logo'
@@ -30,17 +29,10 @@ import Logo from '@components/layout/shared/Logo'
 // Config Imports
 import themeConfig from '@configs/themeConfig'
 
-// Hook Imports
+// Util Imports
 import { useImageVariant } from '@core/hooks/useImageVariant'
 import { useSettings } from '@core/hooks/useSettings'
-import axios from "axios";
-import searchParams from "draft-js/lib/DraftEntity";
-import {router} from "next/client";
-import Cookies from "js-cookie";
-import Box from "@mui/material/Box";
-import {AuthContext} from "@/contexts/AuthContext";
-
-// Util Imports
+import accessControl from "@components/layout/vertical/accessControl";
 
 const schema = object({
   email: string([minLength(1, 'این فیلد الزامی است')]),
@@ -64,9 +56,9 @@ const Login = ({ mode }) => {
   const borderedLightIllustration = '/images/illustrations/auth/v2-login-light-border.png'
 
   // Hooks
-  const { lang: locale } = useParams()
   const { settings } = useSettings()
-  const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext)
+  const router = useRouter()
 
   const {
     control,
@@ -83,108 +75,129 @@ const Login = ({ mode }) => {
   const authBackground = useImageVariant(mode, lightImg, darkImg)
 
   const characterIllustration = useImageVariant(
-    mode,
-    lightIllustration,
-    darkIllustration,
-    borderedLightIllustration,
-    borderedDarkIllustration
+      mode,
+      lightIllustration,
+      darkIllustration,
+      borderedLightIllustration,
+      borderedDarkIllustration
   )
 
   const handleClickShowPassword = () => setIsPasswordShown(show => !show)
-    const router = useRouter()
-  const onSubmit = async data => {
-      await auth.login(data, () => {
 
+  const onSubmit = async data => {
+    await auth.login(data, () => {
+      // After login, you may want to redirect the user to the first page they have access to
+      const workGroup = auth.user?.work_group;
+      const allowedPages = accessControl[workGroup];
+      if (allowedPages && allowedPages.length > 0) {
+        const firstPage = allowedPages[0].href;
+        router.push(firstPage);
+      } else {
+        router.push('/403');
+      }
     })
   }
 
+  useEffect(() => {
+    if (auth.user) {
+      const workGroup = auth.user.work_group;
+      const allowedPages = accessControl[workGroup];
+      if (allowedPages && allowedPages.length > 0) {
+        const firstPage = allowedPages[0].href;
+        router.push(firstPage);
+      } else {
+        router.push('/403');
+      }
+    }
+  }, [auth.user, router])
+
   return (
-    <div className='flex  justify-center items-center min-h-screen'>
-      <div className='flex justify-center items-center  bg-backgroundPaper !min-is-full p-6 md:!min-is-[unset] md:p-12 md:is-[480px]'>
-        <div className='absolute block-start-5 sm:block-start-[38px] inline-start-6 sm:inline-start-[38px]'>
-          <Logo component />
-        </div>
-        <div className='flex flex-col gap-5 is-full sm:is-auto md:is-full sm:max-is-[400px] md:max-is-[unset]'>
-          <div>
-            <Box sx={{ mb: 6,textAlign : "center" }}>
-              <Typography variant='h4'> <span>{themeConfig.templateName}</span></Typography>
-            </Box>
+      <div className='flex justify-center items-center min-h-screen'>
+        <div className='flex justify-center items-center bg-backgroundPaper !min-is-full p-6 md:!min-is-[unset] md:p-12 md:is-[480px]'>
+          <div className='absolute block-start-5 sm:block-start-[38px] inline-start-6 sm:inline-start-[38px]'>
+            <Logo component />
           </div>
-          <form
-            noValidate
-            method='post'
-            autoComplete='off'
-            onSubmit={handleSubmit(onSubmit)}
-            className='flex flex-col gap-5'
-          >
-            <Controller
-              name='email'
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  fullWidth
-                  autoFocus
-                  type='email'
-                  label='نام کاربری'
-                  onChange={e => {
-                    field.onChange(e.target.value)
-                    errorState !== null && setErrorState(null)
-                  }}
-                  {...((errors.email || errorState !== null) && {
-                    error: true,
-                    helperText: errors?.email?.message || errorState?.message[0]
-                  })}
-                />
-              )}
-            />
-            <Controller
-              name='password'
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  fullWidth
-                  label='رمز عبور'
-                  id='login-password'
-                  type={isPasswordShown ? 'text' : 'password'}
-                  onChange={e => {
-                    field.onChange(e.target.value)
-                    errorState !== null && setErrorState(null)
-                  }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position='end'>
-                        <IconButton
-                          edge='end'
-                          onClick={handleClickShowPassword}
-                          onMouseDown={e => e.preventDefault()}
-                          aria-label='toggle password visibility'
-                        >
-                          <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                  }}
-                  {...(errors.password && { error: true, helperText: errors.password.message })}
-                />
-              )}
-            />
-            <div className='flex justify-between items-center flex-wrap gap-x-3 gap-y-1'>
-              <FormControlLabel control={<Checkbox defaultChecked />} label='به خاطر سپردن' />
-              <Typography className='text-end' color='primary' component={Link} href='/forgot-password'>
-                فراموشی رمز عبور
-              </Typography>
+          <div className='flex flex-col gap-5 is-full sm:is-auto md:is-full sm:max-is-[400px] md:max-is-[unset]'>
+            <div>
+              <Box sx={{ mb: 6, textAlign: 'center' }}>
+                <Typography variant='h4'> <span>{themeConfig.templateName}</span></Typography>
+              </Box>
             </div>
-            <Button fullWidth variant='contained' type='submit'>
-              ورود
-            </Button>
-          </form>
+            <form
+                noValidate
+                method='post'
+                autoComplete='off'
+                onSubmit={handleSubmit(onSubmit)}
+                className='flex flex-col gap-5'
+            >
+              <Controller
+                  name='email'
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                      <TextField
+                          {...field}
+                          fullWidth
+                          autoFocus
+                          type='email'
+                          label='نام کاربری'
+                          onChange={e => {
+                            field.onChange(e.target.value)
+                            errorState !== null && setErrorState(null)
+                          }}
+                          {...((errors.email || errorState !== null) && {
+                            error: true,
+                            helperText: errors?.email?.message || errorState?.message[0]
+                          })}
+                      />
+                  )}
+              />
+              <Controller
+                  name='password'
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                      <TextField
+                          {...field}
+                          fullWidth
+                          label='رمز عبور'
+                          id='login-password'
+                          type={isPasswordShown ? 'text' : 'password'}
+                          onChange={e => {
+                            field.onChange(e.target.value)
+                            errorState !== null && setErrorState(null)
+                          }}
+                          InputProps={{
+                            endAdornment: (
+                                <InputAdornment position='end'>
+                                  <IconButton
+                                      edge='end'
+                                      onClick={handleClickShowPassword}
+                                      onMouseDown={e => e.preventDefault()}
+                                      aria-label='toggle password visibility'
+                                  >
+                                    <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
+                                  </IconButton>
+                                </InputAdornment>
+                            )
+                          }}
+                          {...(errors.password && { error: true, helperText: errors.password.message })}
+                      />
+                  )}
+              />
+              <div className='flex justify-between items-center flex-wrap gap-x-3 gap-y-1'>
+                <FormControlLabel control={<Checkbox defaultChecked />} label='به خاطر سپردن' />
+                <Typography className='text-end' color='primary' component={Link} href='/forgot-password'>
+                  فراموشی رمز عبور
+                </Typography>
+              </div>
+              <Button fullWidth variant='contained' type='submit'>
+                ورود
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
 
