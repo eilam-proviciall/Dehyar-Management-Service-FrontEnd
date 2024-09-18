1f1904db719a3fd1946e36845f4afc6395ebc0d2
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-09-18 15:32:55 +0330

    refactor: dynamically load icons based on JSON data using getIconByName function

--- src/views/dehyari/form/edit/PhoneNumberStepContact.jsx
+++ src/views/dehyari/form/edit/PhoneNumberStepContact.jsx
@@ -12,6 +12,8 @@ import ShadIcon from '@mui/icons-material/School';
 import DividerSimple from "@components/common/Divider/DividerSimple";
 import InputAdornment from "@mui/material/InputAdornment";
 import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import Autocomplete from "@mui/material/Autocomplete";
+import socialNetworks from "@data/socialNetworks.json"
 const PhoneNumberStepContact = ({ validation }) => {
     const { control, watch, formState: { errors } } = useFormContext();
     const { fields, append, remove } = useFieldArray({
@@ -36,13 +38,20 @@ const PhoneNumberStepContact = ({ validation }) => {
         }
     }, [fields, append]);
 
-    const socialNetworks = [
-        { value: 'eitaa', label: 'ایتا' },
-        { value: 'bale', label: 'بله' },
-        { value: 'shad', label: 'شاد' },
-        { value: 'contactPhone', label: 'شماره ضروری' }
-    ];
+    const getIconByName = (iconName) => {
+        const icons = {
+            EitaaIcon: <EitaaIcon />,
+            BaleIcon: <BaleIcon />,
+            ShadIcon: <ShadIcon />,
+            RubikaIcon: <ShadIcon />,
+            IGapIcon: <ShadIcon />,
+            TelegramIcon: <ShadIcon />,
+            WhatsappIcon: <ShadIcon />,
+            ContactPhoneIcon: <ContactPhoneIcon />
+        };
 
+        return icons[iconName] || null; // برگرداندن آیکون بر اساس نام
+    };
     return (
         <Grid container spacing={2} mt={1}>
             <Grid item xs={12}>
@@ -58,7 +67,7 @@ const PhoneNumberStepContact = ({ validation }) => {
                             <Card key={item.id} sx={{ mb: 2 }}>
                                 <CardContent>
                                     <Grid container spacing={2}>
-                                        <Grid item xs={12} sm={6}>
+                                        <Grid item xs={12} sm={4}>
                                             <Controller
                                                 name={`contacts[${index}].phoneNumber`}
                                                 control={control}
@@ -82,55 +91,51 @@ const PhoneNumberStepContact = ({ validation }) => {
                                             />
                                         </Grid>
 
-                                        <Grid item xs={12} sm={6}>
+                                        <Grid item xs={12} sm={8}>
                                             <Controller
                                                 name={`contacts[${index}].socialNetwork`}
                                                 control={control}
                                                 defaultValue={item.socialNetwork}
                                                 render={({ field }) => (
-                                                    <FormControl fullWidth size="small">
-                                                        <InputLabel id={`social-network-label-${index}`}>شبکه اجتماعی</InputLabel>
-                                                        <Select
-                                                            labelId={`social-network-label-${index}`}
-                                                            label="شبکه اجتماعی"
-                                                            multiple // افزودن این ویژگی برای چند انتخابی
-                                                            value={field.value || []}
-                                                            onChange={field.onChange}
-                                                            renderValue={(selected) => (
-                                                                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                                                    {selected.map((value) => {
-                                                                        const network = socialNetworks.find((network) => network.value === value);
-                                                                        return <Chip key={value} label={network ? network.label : value} />;
-                                                                    })}
-                                                                </Box>
-                                                            )}
-                                                        >
-                                                            <MenuItem value="eitaa">
-                                                                <IconButton edge="end" disabled>
-                                                                    <EitaaIcon />
+                                                    <Autocomplete
+                                                        multiple
+                                                        options={socialNetworks}
+                                                        disableCloseOnSelect={true}
+                                                        getOptionLabel={(option) => option.label}
+                                                        value={socialNetworks.filter((option) => field.value.includes(option.value))}
+                                                        onChange={(_, newValue) => {
+                                                            field.onChange(newValue.map((option) => option.value));
+                                                        }}
+                                                        renderOption={(props, option) => (
+                                                            <li {...props}>
+                                                                <IconButton edge="end" disabled
+                                                                            style={{marginLeft: 1}}> {/* اضافه کردن فاصله */}
+                                                                    {getIconByName(option.icon)}
                                                                 </IconButton>
-                                                                <Typography sx={{ mr: 'auto' }}>ایتا</Typography>
-                                                            </MenuItem>
-                                                            <MenuItem value="bale">
-                                                                <IconButton edge="end" disabled>
-                                                                    <BaleIcon />
-                                                                </IconButton>
-                                                                <Typography sx={{ mr: 'auto' }}>بله</Typography>
-                                                            </MenuItem>
-                                                            <MenuItem value="shad">
-                                                                <IconButton edge="end" disabled>
-                                                                    <ShadIcon />
-                                                                </IconButton>
-                                                                <Typography sx={{ mr: 'auto' }}>شاد</Typography>
-                                                            </MenuItem>
-                                                            <MenuItem value="contactPhone">
-                                                                <IconButton edge="end" disabled>
-                                                                    <ContactPhoneIcon />
-                                                                </IconButton>
-                                                                <Typography sx={{ mr: 'auto' }}>شماره ضروری</Typography>
-                                                            </MenuItem>
-                                                        </Select>
-                                                    </FormControl>
+                                                                <Typography
+                                                                    sx={{flexGrow: 1}}>{option.label}</Typography>
+                                                            </li>
+                                                        )}
+                                                        renderTags={(selected, getTagProps) => (
+                                                            <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                                                            {selected.map((option, index) => (
+                                                                    <Chip
+                                                                        key={option.value}
+                                                                        label={option.label}
+                                                                        {...getTagProps({ index })}
+                                                                    />
+                                                                ))}
+                                                            </Box>
+                                                        )}
+                                                        renderInput={(params) => (
+                                                            <TextField
+                                                                {...params}
+                                                                label="شبکه اجتماعی"
+                                                                size="small"
+                                                                placeholder="انتخاب شبکه‌های اجتماعی"
+                                                            />
+                                                        )}
+                                                    />
                                                 )}
                                             />
                                         </Grid>
