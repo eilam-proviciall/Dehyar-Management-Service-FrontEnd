5d4e21184c11a112a72f4554b645c1659b66892c
amin dehnavi <amin1384dehnavi@gmail.com>
2024-09-30 19:56:17 +0330

    Complete Region Cell

---
+++ src/views/municipality/list/cells/RegionCell.jsx
@@ -0,0 +1,31 @@
+import { getGeoData } from '@/Services/DataService';
+import api from '@/utils/axiosInstance';
+import React, { useEffect, useState } from 'react'
+
+const RegionCell = ({ region }) => {
+    const [currentRegion, setCurrentRegion] = useState('');
+
+    useEffect(() => {
+        console.log("State => ", region);
+
+        const fetchData = async () => {
+            try {
+                const regionResponse = await api.get(`${getGeoData()}?level=region&hierarchy_code=${region}`, { requiresAuth: true });
+                console.log("Regions => ", regionResponse);
+
+                setCurrentRegion(regionResponse?.data[0]?.approved_name || '');
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [region]);
+
+    return (
+        <div style={{ textAlign: 'right' }}>{currentRegion || ''}</div>
+    );
+
+}
+
+export default RegionCell
