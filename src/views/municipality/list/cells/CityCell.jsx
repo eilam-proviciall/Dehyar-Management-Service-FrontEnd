da03e37e541da5db952c9474408022e09ad2e260
amin dehnavi <amin1384dehnavi@gmail.com>
2024-09-30 19:56:31 +0330

    Complete City Cell

---
+++ src/views/municipality/list/cells/CityCell.jsx
@@ -0,0 +1,29 @@
+import { getGeoData } from '@/Services/DataService';
+import api from '@/utils/axiosInstance';
+import React, { useEffect, useState } from 'react'
+
+const CityCell = ({ city }) => {
+    const [currentCity, setCurrentCity] = useState('');
+
+    useEffect(() => {
+        console.log("State => ", city);
+
+        const fetchData = async () => {
+            try {
+                const cityResponse = await api.get(`${getGeoData()}?level=city&hierarchy_code=${city}`, { requiresAuth: true });
+                setCurrentCity(cityResponse?.data[0]?.approved_name || '');
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [city]);
+
+    return (
+        <div style={{ textAlign: 'right' }}>{currentCity || ''}</div>
+    );
+
+}
+
+export default CityCell
