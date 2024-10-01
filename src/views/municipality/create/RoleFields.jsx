d527849c6a9c731deb3cde3a63fcaf75dba428a6
amin dehnavi <amin1384dehnavi@gmail.com>
2024-10-01 23:41:24 +0330

    Fix add  functionality for financial officer and district head

--- src/views/municipality/create/RoleFields.jsx
+++ src/views/municipality/create/RoleFields.jsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 
-const RoleFields = ({ role, control, errors, isLoading, options, selectedOptions, setSidebarDetails }) => {
+const RoleFields = ({ role, control, errors, isLoading, options, selectedOptions, sidebarDetails, setSidebarDetails }) => {
 
     if (role && (!options || options.length === 0)) {
         return <Typography variant='body1'>در حال دریافت داده ها...</Typography>;
@@ -30,13 +30,13 @@ const RoleFields = ({ role, control, errors, isLoading, options, selectedOptions
                                     disableCloseOnSelect
                                     getOptionLabel={(option) => `${option.city.approved_name}-${option.approved_name}`}
                                     onChange={(event, newValue) => {
-                                        setSidebarDetails(prevState => ({
+                                        sidebarDetails.status == 'edit' && (setSidebarDetails(prevState => ({
                                             ...prevState,
                                             defaultValues: {
                                                 ...prevState.defaultValues,
                                                 geo_region: newValue.hierarchy_code
                                             }
-                                        }));
+                                        })));
                                         onChange(newValue || null);
                                     }}
                                     defaultValue={
@@ -77,13 +77,13 @@ const RoleFields = ({ role, control, errors, isLoading, options, selectedOptions
                                     options={options}
                                     getOptionLabel={(option) => `${option.city_name}-${option.approved_name}`}
                                     onChange={(event, newValue) => {
-                                        setSidebarDetails(prevState => ({
+                                        sidebarDetails.status == 'edit' && (setSidebarDetails(prevState => ({
                                             ...prevState,
                                             defaultValues: {
                                                 ...prevState.defaultValues,
                                                 covered_villages: newValue.map(item => item.hierarchy_code || [])
                                             }
-                                        }));
+                                        })));
                                         onChange(newValue.map(item => item || []));
                                     }}
                                     defaultValue={
