dec1e4f3521cc80881ac44c62cdc05703290dab4
amin <68153198+amindehnavi2005@users.noreply.github.com>
2024-10-08 11:33:51 +0330

    change footer humadResource pdf

--- src/@core/components/loading/Loading.jsx
+++ src/@core/components/loading/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className='flex h-full w-full justify-center items-center'>
-            <img className='w-[25%]' src="/images/icons/logo-spiner.svg" alt="در حال بارگذاری..." />
+            <img className='w-[25%]' src="/images/icons/spier.svg" alt="در حال بارگذاری..." />
         </div>
     );
 }
