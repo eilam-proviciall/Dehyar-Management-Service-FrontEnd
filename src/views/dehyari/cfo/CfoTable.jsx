4f4d6d28472640d6b015ee722028a1ac85571761
amin dehnavi <amin1384dehnavi@gmail.com>
2024-10-15 10:41:46 +0330

    change actions icons

--- src/views/dehyari/cfo/CfoTable.jsx
+++ src/views/dehyari/cfo/CfoTable.jsx
@@ -1,19 +1,19 @@
 "use client"
-import React, { useEffect, useMemo, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { MaterialReactTable } from 'material-react-table';
+import React, {useEffect, useMemo, useState} from 'react';
+import {useRouter} from 'next/navigation';
+import {MaterialReactTable, useMaterialReactTable} from 'material-react-table';
 import Chip from "@mui/material/Chip";
-import { IconButton, Menu, MenuItem } from '@mui/material';
-import { DownloadHumanResourcePdf, GetHumanResourcesForCfo } from "@/Services/humanResources";
+import {IconButton, Menu, MenuItem} from '@mui/material';
+import {DownloadHumanResourcePdf, GetHumanResourcesForCfo} from "@/Services/humanResources";
 import contractType from "@data/contractType.json";
 import PersonalOption from "@data/PersonalOption.json";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Link from 'next/link';
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
 import MyDocument from "@components/MyDocument";
-import { pdf } from "@react-pdf/renderer";
+import {pdf} from "@react-pdf/renderer";
 import HumanResourceDTO from "@/utils/HumanResourceDTO";
-import { getJobTitleLabel } from "@data/jobTitles";
+import {getJobTitleLabel} from "@data/jobTitles";
 import api from '@/utils/axiosInstance';
 import Loading from '@/@core/components/loading/Loading';
 
@@ -34,20 +34,22 @@ function CfoTable(props) {
     // دانلود فایل PDF
     const handleDownloadPdf = async (row) => {
         try {
-            const response = await api.get(`${DownloadHumanResourcePdf()}?human_resource_id=${row.human_resource_id}`, { requiresAuth: true });
+            const response = await api.get(`${DownloadHumanResourcePdf()}?human_resource_id=${row.human_resource_id}`, {requiresAuth: true});
 
             const humanResourceData = response.data;
             console.log(humanResourceData)
             const data = new HumanResourceDTO(humanResourceData);
-            const doc = <MyDocument data={data} />;
+            const doc = <MyDocument data={data}/>;
             const asPdf = pdf([]);
             asPdf.updateContainer(doc);
             const blob = await asPdf.toBlob();
             const url = URL.createObjectURL(blob);
             window.open(url, '_blank');
 
-            toast.success('محاسبه موفق بود', { position: "top-center" });
-        } catch (error) { return error }
+            toast.success('محاسبه موفق بود', {position: "top-center"});
+        } catch (error) {
+            return error
+        }
     };
 
     // بستن منو
@@ -59,7 +61,7 @@ function CfoTable(props) {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await api.get(`${GetHumanResourcesForCfo()}`, { requiresAuth: true });
+                const response = await api.get(`${GetHumanResourcesForCfo()}`, {requiresAuth: true});
                 setData(response.data);
                 setLoading(false);
             } catch (error) {
@@ -80,38 +82,38 @@ function CfoTable(props) {
                 accessorKey: 'village',
                 header: 'دهیاری',
                 size: 150,
-                Cell: ({ cell }) => <div style={{ textAlign: 'right' }}>{cell.getValue().approved_name}</div>,
+                Cell: ({cell}) => <div style={{textAlign: 'right'}}>{cell.getValue().approved_name}</div>,
             },
             {
                 accessorKey: 'first_name',
                 header: 'نام و نام خانوادگی',
                 size: 150,
-                Cell: ({ row }) => {
-                    const { first_name, last_name } = row.original;
-                    return <div style={{ textAlign: 'right' }}>{`${first_name ?? " "} ${last_name ?? " "}`}</div>;
+                Cell: ({row}) => {
+                    const {first_name, last_name} = row.original;
+                    return <div style={{textAlign: 'right'}}>{`${first_name ?? " "} ${last_name ?? " "}`}</div>;
                 },
             },
             {
                 accessorKey: 'job_type',
                 header: 'پست سازمانی',
                 size: 150,
-                Cell: ({ cell }) => <div style={{ textAlign: 'right' }}>{cell.getValue()}</div>,
+                Cell: ({cell}) => <div style={{textAlign: 'right'}}>{cell.getValue()}</div>,
             },
             {
                 accessorKey: 'nid',
                 header: 'کدملی',
                 size: 150,
-                Cell: ({ cell }) => <div style={{ textAlign: 'right' }}>{cell.getValue()}</div>,
+                Cell: ({cell}) => <div style={{textAlign: 'right'}}>{cell.getValue()}</div>,
             },
             {
                 accessorKey: 'contract_type',
                 header: 'نوع قرار داد',
                 size: 150,
-                Cell: ({ cell }) => {
+                Cell: ({cell}) => {
                     const role = cell.getValue();
                     return (
-                        <div style={{ textAlign: 'right' }}>
-                            <Chip label={contractType[role]} color="primary" />
+                        <div style={{textAlign: 'right'}}>
+                            <Chip label={contractType[role]} color="primary"/>
                         </div>
                     );
                 },
@@ -120,18 +122,18 @@ function CfoTable(props) {
                 accessorKey: 'actions',
                 header: 'عملیات',
                 size: 150,
-                Cell: ({ row }) => (
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                Cell: ({row}) => (
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
                         <IconButton
                             aria-label="more"
                             aria-controls={open ? 'long-menu' : undefined}
                             aria-expanded={open ? 'true' : undefined}
                             aria-haspopup="true"
                             onClick={(event) => handleClick(event, row)}
-                            style={{ paddingLeft: 0 }}
+                            style={{paddingLeft: 0}}
                         >
                             <MoreVertIcon
-                                style={{ textAlign: "center", justifyContent: 'center', alignItems: 'center' }} />
+                                style={{textAlign: "center", justifyContent: 'center', alignItems: 'center'}}/>
                         </IconButton>
                         <Menu
                             id="long-menu"
@@ -162,15 +164,26 @@ function CfoTable(props) {
         [anchorEl, currentRow]
     );
 
+    const table = useMaterialReactTable({
+        columns,
+        data: tableData,
+        initialState: {
+            density: 'compact'
+        },
+        // muiTableHeadCellProps: {
+        //     className: 'bg-gray-200 py-2'
+        // },
+        muiTableBodyCellProps: {
+            className: 'bg-white'
+        }
+    })
+
     if (loading) {
         return <Loading/>
     }
 
     return (
-        <MaterialReactTable
-            columns={columns}
-            data={tableData}
-        />
+        <MaterialReactTable table={table}/>
     );
 }
 
