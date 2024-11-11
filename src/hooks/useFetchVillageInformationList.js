2360c68fc65d70deb0f9e2a3ec63f1f6b743591f
amin dehnavi <amin1384dehnavi@gmail.com>
2024-11-11 18:45:43 +0330

    fix bug show coveredVillages by admin

--- src/hooks/useFetchVillageInformationList.js
+++ src/hooks/useFetchVillageInformationList.js
@@ -1,8 +1,8 @@
-import {useState, useEffect} from 'react';
-import {getVillageInformationList} from "@/Services/CountryDivision";
+import { useState, useEffect } from 'react';
+import { getVillageInformationList } from "@/Services/CountryDivision";
 import api from '@/utils/axiosInstance';
 
-export const useFetchVillageInformationList = (shouldFetchCities) => {
+export const useFetchVillageInformationList = (shouldFetchCities, userData) => {
     const [villages, setVillages] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -10,19 +10,27 @@ export const useFetchVillageInformationList = (shouldFetchCities) => {
     useEffect(() => {
         const fetchVillages = async () => {
             setIsLoading(true);
-            await api.get(getVillageInformationList(), {requiresAuth: true})
-                .then(response => {
-                    console.log("Response Villages => ",response.data);
-                    setVillages(response.data.filter(village=> village.geo_state === userData));
-                })
-                .catch(err => setError(err))
-                .finally(setIsLoading(false));
+            try {
+                const response = await api.get(getVillageInformationList(), { requiresAuth: true });
+                const data = response.data;
+
+                if (userData) {
+                    setVillages(data.filter(village => village.geo_state === userData));
+                } else {
+                    setVillages(data);
+                }
+            } catch (err) {
+                setError(err);
+            } finally {
+                setIsLoading(false);
+            }
         };
 
         if (shouldFetchCities) {
             fetchVillages();
         }
 
-    }, [shouldFetchCities]);
-    return {villages, isLoading, error};
+    }, [shouldFetchCities, userData]);
+
+    return { villages, isLoading, error };
 };
