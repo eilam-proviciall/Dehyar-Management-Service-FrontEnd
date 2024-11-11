c89657044539b92b5b489c287bb2d225d5c277cf
amin dehnavi <amin1384dehnavi@gmail.com>
2024-11-11 17:59:32 +0330

    add new loading for dark mode website

--- src/@core/components/loading/Loading.jsx
+++ src/@core/components/loading/Loading.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
 
 const Loading = () => {
+    const theme = useTheme();
+    const isDarkMode = theme.palette.mode === 'dark';
+
     return (
         <div className='flex h-full w-full justify-center items-center'>
-            <img className='w-[15%]' src="/images/icons/spier.svg" alt="در حال بارگذاری..." />
+            <img
+                className='w-[15%]'
+                src={isDarkMode ? "/images/icons/spier-dark.svg" : "/images/icons/spier.svg"}
+                alt="در حال بارگذاری..."
+            />
         </div>
     );
 }
 
-export default Loading
+export default Loading;
