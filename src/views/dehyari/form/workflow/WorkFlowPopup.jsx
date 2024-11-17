f6b4075e9376ef1f2f1b8ad96cb24070d7073344
amin dehnavi <amin1384dehnavi@gmail.com>
2024-11-17 09:36:58 +0330

    change ui WorkFlowPopup.jsx

--- src/views/dehyari/form/workflow/WorkFlowPopup.jsx
+++ src/views/dehyari/form/workflow/WorkFlowPopup.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import {Dialog, DialogActions, DialogContent, DialogTitle, Button, IconButton} from '@mui/material';
+import React, { useState } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, IconButton, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from "@mui/material/Typography";
-import api from "@utils/axiosInstance";
-import {ChangeStateSalaries} from "@/Services/humanResources";
+import { approveWorkflow, rejectWorkflow } from "@/utils/workflowService";
 
-const WorkFlowPopup = ({open, setOpen, information}) => {
+const WorkFlowPopup = ({ open, setOpen, information }) => {
     const handleClose = () => {
         setOpen(false);
+    };
+
+    const handleApprove = () => {
+        approveWorkflow(information.human_resource_id,'pending_supervisor','')
+            .then(response => {
+                console.log("Response => ", response);
+            })
+            .catch(error => {
+                console.error("Error => ", error);
+            });
+        handleClose();
     };
 
     return (
@@ -23,27 +33,19 @@ const WorkFlowPopup = ({open, setOpen, information}) => {
                         color: (theme) => theme.palette.grey[500],
                     }}
                 >
-                    <CloseIcon/>
+                    <CloseIcon />
                 </IconButton>
             </DialogTitle>
             <DialogContent>
-                <Typography variant={'h6'} className={'p-5'}>آیا از ارسال حکم به بخشداری جهت بررسی اطمینان
-                    دارید؟</Typography>
+                <Typography variant={'h6'} className={'pt-5'}>
+                    آیا از ارسال حکم به بخشداری جهت بررسی اطمینان دارید؟
+                </Typography>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="primary">
                     انصراف
                 </Button>
-                <Button onClick={() => {
-                    const finallyData = {
-                        state: 'pending_bakhshdar',
-                        message: 'در انتظار تایید بخشدار',
-                    };
-                    console.log("Information => ", information)
-                    const response = api.post(`${ChangeStateSalaries()}/${information.human_resource_id}`, finallyData, {requiresAuth: true});
-                    console.log("Response => ", response);
-                    handleClose();
-                }} color="primary">
+                <Button onClick={handleApprove} color="primary">
                     تایید
                 </Button>
             </DialogActions>
