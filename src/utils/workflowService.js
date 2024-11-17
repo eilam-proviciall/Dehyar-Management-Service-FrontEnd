5f6f716997f8d295eb107eaad42aa3517f96e1fb
amin dehnavi <amin1384dehnavi@gmail.com>
2024-11-17 09:36:47 +0330

    create workflowService.js in utils

---
+++ src/utils/workflowService.js
@@ -0,0 +1,19 @@
+import api from "@utils/axiosInstance";
+import { ChangeStateSalaries } from "@/Services/humanResources";
+
+export const approveWorkflow = (humanResourceId, state,message) => {
+    const data = {
+        state: state,
+        message: message,
+    };
+    console.log("Data => ", data);
+    return api.post(ChangeStateSalaries(humanResourceId), data, { requiresAuth: true });
+};
+
+export const rejectWorkflow = (humanResourceId,state, message) => {
+    const data = {
+        state: state,
+        message: message,
+    };
+    return api.post(ChangeStateSalaries(humanResourceId), data, { requiresAuth: true });
+};
