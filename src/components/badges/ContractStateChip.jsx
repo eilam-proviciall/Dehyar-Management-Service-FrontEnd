3bf4d94242d63c235037d90fffc5a0126e945b97
amin dehnavi <amin1384dehnavi@gmail.com>
2024-11-18 18:32:23 +0330

    change ContractStateChip.jsx

--- src/components/badges/ContractStateChip.jsx
+++ src/components/badges/ContractStateChip.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Chip from '@mui/material/Chip';
 import Avatar from "@mui/material/Avatar";
+import {useSettings} from "@core/hooks/useSettings";
 
 const ContractStateChip = ({ avatar,label, onClick, customStyles }) => {
+    const {settings} = useSettings();
     return (
         <Chip
-            avatar={<Avatar>{avatar || <i className={'ri-close-fill'}/>}</Avatar>}
+            avatar={<Avatar className={`${avatar == "30" && "bg-success text-white" || avatar && "bg-backgroundPaper" || "bg-error text-white"}`}>{avatar == '30' && < i class="ri-check-line"/> || avatar || <i className={'ri-close-fill'}/>}</Avatar>}
             label={label}
             onClick={onClick}
-            style={{ textAlign: 'center', alignItems:'center', ...customStyles }}
+            style={{ textAlign: 'right', ...customStyles }}
         />
     );
 };
