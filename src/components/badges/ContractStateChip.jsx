354c505fafe54a2f72708a82307e03f8102cc9f8
amin dehnavi <amin1384dehnavi@gmail.com>
2024-11-20 08:08:06 +0330

    remove shadow in ContractStateChip.jsx

--- src/components/badges/ContractStateChip.jsx
+++ src/components/badges/ContractStateChip.jsx
@@ -7,10 +7,9 @@ const ContractStateChip = ({ avatar,label, onClick, customStyles }) => {
     const {settings} = useSettings();
     return (
         <Chip
-            avatar={<Avatar className={`${avatar == "30" && "bg-success text-white" || avatar && "bg-backgroundPaper" || "bg-error text-white"}`}>{avatar == '30' && < i class="ri-check-line"/> || avatar || <i className={'ri-close-fill'}/>}</Avatar>}
+            avatar={<Avatar className={`flex items-center pt-1 ${avatar == "30" && "bg-success text-white" || avatar && "bg-backgroundPaper" || "bg-error text-white"}`}>{avatar || <i className={'ri-close-fill'}/>}</Avatar>}
             label={label}
             onClick={onClick}
-            className={'shadow-xs'}
             style={{ textAlign: 'right', ...customStyles }}
         />
     );
