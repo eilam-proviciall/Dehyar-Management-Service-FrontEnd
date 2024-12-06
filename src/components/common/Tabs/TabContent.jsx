a9eaf684c3c4aa0b44a7efc2b6169cc3e525de1b
amin dehnavi <amin1384dehnavi@gmail.com>
2024-12-06 11:44:40 +0330

    fix bug scroll in tabContent

--- src/components/common/Tabs/TabContent.jsx
+++ src/components/common/Tabs/TabContent.jsx
@@ -7,8 +7,11 @@ const TabContent = ({ active, children }) => {
             sx={{
                 opacity: active ? 1 : 0,
                 transform: `translateY(${active ? 0 : '10px'})`,
-                position: active ? 'relative' : 'absolute',
+                position: active ? 'static' : 'absolute',
+                visibility: active ? 'visible' : 'hidden',
                 width: '100%',
+                height: active ? 'auto' : 0,
+                overflow: 'hidden',
                 transition: 'opacity 0.3s ease, transform 0.3s ease',
             }}
         >
