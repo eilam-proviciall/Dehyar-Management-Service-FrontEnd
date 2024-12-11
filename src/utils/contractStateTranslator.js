52af553f042119c0532a01e4340b43fc7c50a8a5
amin dehnavi <amin1384dehnavi@gmail.com>
2024-12-11 10:05:40 +0330

    add color to contractStateTranslator

--- src/utils/contractStateTranslator.js
+++ src/utils/contractStateTranslator.js
@@ -1,22 +1,49 @@
 export const translateContractState = (state) => {
-    switch (state) {
-        case 'draft':
-            return 'پیش نویس';
-        case 'pending_supervisor':
-            return 'در انتظار تایید بخشدار';
-        case 'approved_by_supervisor':
-            return 'تایید شده توسط بخشدار';
-        case 'rejected_by_supervisor':
-            return 'رد شده توسط بخشدار';
-        case 'pending_governor':
-            return 'در انتظار تایید استانداری';
-        case 'approved':
-            return 'تایید شده';
-        case 'rejected_to_supervisor':
-            return 'بازگشت به بخشدار';
-        case 'rejected_to_financial_officer':
-            return 'نیازمند اصلاح مجدد';
-        default:
-            return 'نامشخص';
-    }
+  switch (state) {
+    case "draft":
+      return {
+        title: "پیش نویس",
+        color: "info",
+      };
+    case "pending_supervisor":
+      return {
+        title: "در انتظار تایید بخشدار",
+        color: "warning",
+      };
+    case "approved_by_supervisor":
+      return {
+        title: "تایید شده توسط بخشدار",
+        color: "primary",
+      };
+    case "rejected_by_supervisor":
+      return {
+        title: "رد شده توسط بخشدار",
+        color: "error",
+      };
+    case "pending_governor":
+      return {
+        title: "در انتظار تایید استانداری",
+        color: "primary",
+      };
+    case "approved":
+      return {
+        title: "تایید شده",
+        color: "success",
+      };
+    case "rejected_to_supervisor":
+      return {
+        title: "بازگشت به بخشدار",
+        color: "error",
+      };
+    case "rejected_to_financial_officer":
+      return {
+        title: "نیازمند اصلاح مجدد",
+        color: "error",
+      };
+    default:
+      return {
+        title: "نامشخص",
+        color: "error",
+      };
+  }
 };
